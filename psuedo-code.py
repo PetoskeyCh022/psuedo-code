1dac489a1954acc1b685070805aa0765a92c3935
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2024-09-23 11:04:08 -0400

    updated psuedo code

---
+++ psuedo-code.py
@@ -0,0 +1,4 @@
+a_leg = int(input("Enter the A leg value of your right triangle: "))
+b_leg = int(input("Enter the B leg value of your right triangle: "))
+area = a_leg * b_leg / 2
+print("The area of your right triangle is "+ str(area) + ".")
