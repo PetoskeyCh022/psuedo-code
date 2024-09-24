e85b3e1d348db135aa7889724e69f0e0681479ea
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2024-09-24 11:03:10 -0400

    Updated psuedo-code.py

--- psuedo-code.py
+++ psuedo-code.py
@@ -2,7 +2,19 @@
 # 9/23/24
 # Psuedo Code
 
-a_leg = int(input("Enter the A leg value of your right triangle: "))
-b_leg = int(input("Enter the B leg value of your right triangle: "))
-area = a_leg * b_leg / 2
-print("The area of your right triangle is "+ str(area) + ".")
+#a_leg = int(input("Enter the A leg value of your right triangle: "))
+#b_leg = int(input("Enter the B leg value of your right triangle: "))
+#area = a_leg * b_leg / 2
+#print("The area of your right triangle is "+ str(area) + ".")
+
+# Charles
+# 9/24/24
+# Psuedo Code
+
+first_name = input("Enter your first name:\n")
+miles_driven_today = float(input("Enter the distance you drove today in miles:\n"))
+gallons_used_today = float(input("Enter the number of gallons used today:\n"))
+mpg = miles_driven_today / gallons_used_today
+print(f'{first_name.title()} has driven {miles_driven_today} miles today.')
+print(f'{first_name.title()} has used {gallons_used_today} gallons today.')
+print(f'{first_name.title()}\'s car can drive {mpg} miles per gallon.')
